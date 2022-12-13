9ceafafc66ae9c56470ba3a81c7bad862d05b8f6
OEUNN <jyejyp5@gmail.com>
2022-12-13 17:36:02 +0900

    초기파일

---
+++ src/com/coderby/myapp/di/HelloController.java
@@ -0,0 +1,22 @@
+package com.coderby.myapp.di;
+
+public class HelloController {
+	
+	//객체를 저장할 수 있도록 인터페이스로 선언만 함
+	IHelloService helloService;
+	
+	/*
+	//필요한 객체를 전달받을 수 있도록 생성자 추가
+	public HelloController(IHelloService helloService) {
+		this.helloService = helloService;
+	}*/
+	
+	//필요한 객체를 전달받을 수 있도록 set 메서드 추가
+	public void setHelloService(IHelloService helloService) {
+		this.helloService = helloService;
+	}
+	
+	public void hello(String name) {
+		System.out.println("HelloController :"+helloService.sayHello(name));
+	}
+}
---
+++ src/com/coderby/myapp/di/HelloMain.java
@@ -0,0 +1,12 @@
+package com.coderby.myapp.di;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		IHelloService helloService = new HelloService();
+		HelloController controller = new HelloController();
+		controller.setHelloService(helloService);
+		controller.hello("홍길동");
+	}
+
+}
